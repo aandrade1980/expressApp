cc83c65509f8f6f3cbe505aa7cc3edad5510a712
Alvaro Andrade <varito1@gmail.com>
2020-11-26 18:04:54 -0300

    Added passport setup

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+require('./passport_setup')(passport);
 
 var app = express();
 
@@ -19,16 +23,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: 'our new secret' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ passport_setup.js
@@ -0,0 +1,56 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const models = require('./models');
+
+const validPassword = (user, password) => {
+  return bcrypt.compareSync(password, user.password);
+};
+
+module.exports = passport => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    models.User.findOne({
+      where: {
+        id
+      }
+    }).then(user => {
+      if (user === null) {
+        done(new Error('Wrong user id!'));
+      }
+      done(null, user);
+    });
+  });
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+      },
+      function (req, email, password, done) {
+        return models.User.findOne({
+          where: {
+            email
+          }
+        })
+          .then(user => {
+            if (user === null) {
+              req.flash('message', 'Incorrect credentials!');
+              return done(null, false);
+            } else if (!user.password) {
+              req.flash('message', 'You must reset your password');
+              return done(null, false);
+            } else if (!validPassword(user, password)) {
+              req.flash('message', 'Incorrect credentials!');
+              return done(null, false);
+            }
+
+            return done(null, user);
+          })
+          .catch(err => done(err, false));
+      }
+    )
+  );
+};
