7bcdacef9edbf356aaaff3f8cd12b01c02cf143d
Alvaro Andrade <varito1@gmail.com>
2020-11-26 17:47:29 -0300

    Added passport passport-local bcrypt validator express-session connect-flash

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,22 @@ var router = express.Router();
 const landing = require('../controllers/landing');
 const user = require('../controllers/user');
 
+/* USER - GETS */
 router.get('/login', user.show_login);
 router.get('/signup', user.show_signup);
 
+/* USER - POSTS */
+router.post('/signup', user.signup);
+
 /* GET home page. */
 router.get('/', landing.get_landing);
+
+/* LEADS - GETS */
 router.get('/leads', landing.show_leads);
 router.get('/lead/:lead_id', landing.show_lead);
 router.get('/lead/:lead_id/edit', landing.show_edit_lead);
 
+/* LEADS - POSTS */
 router.post('/', landing.submit_lead);
 router.post('/lead/:lead_id/edit', landing.edit_lead);
 router.post('/lead/:lead_id/delete', landing.delete_lead);
