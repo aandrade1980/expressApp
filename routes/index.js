f949d2b96d5699573cc24049d9b9303a13607e64
Alvaro Andrade <varito1@gmail.com>
2020-11-26 17:43:21 -0300

    Added User model, migration User table

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const landing = require('../controllers/landing');
+const user = require('../controllers/user');
+
+router.get('/login', user.show_login);
+router.get('/signup', user.show_signup);
 
 /* GET home page. */
 router.get('/', landing.get_landing);
